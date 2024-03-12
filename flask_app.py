c265b1e3f13bf08196c9a2ba8ca43bcca3b4958f
Guilherme Faga <faga@retize.com.br>
2024-03-12 17:52:00 -0300

    build: add post-merge hook and update Flask app

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,21 @@
+from flask import Flask, request
+import git
 
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask and Github Actions!'
+    return 'Hello from Flask!'
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
