3e0d7a13329a4c3de93cf94eac141ab96375cb68
Guilherme Faga <faga@retize.com.br>
2024-03-12 18:33:31 -0300

    update: implement signature verification in webhook endpoint

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request
 from git import Repo
+from dotenv import load_dotenv
+
+from .check_signature import is_valid_signature
+
 import traceback
+import os
+
+
+load_dotenv('./.env')
 
 
 app = Flask(__name__)
@@ -14,6 +22,18 @@ def hello_world():
 @app.route('/update_server', methods=['POST'])
 def webhook():
     try:
+        GITHUB_WEBHOOK_TOKEN = os.getenv("GITHUB_WEBHOOK_TOKEN")
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+
+        if x_hub_signature is None:
+            return 'Missing X-Hub-Signature', 400
+        
+        if not x_hub_signature.startswith('sha1='):
+            return 'Invalid X-Hub-Signature', 400
+        
+        if not is_valid_signature(x_hub_signature, request.data, GITHUB_WEBHOOK_TOKEN):
+            return 'Invalid X-Hub-Signature', 400
+        
         repo = Repo('./mysite')
         git = repo.git
         git.checkout('main')
